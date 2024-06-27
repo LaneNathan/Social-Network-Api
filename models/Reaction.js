5b218cca2d096168ab7ba189fb5e489ab56fecd9
Nathan Lane <lanenathan214@gmail.com>
2024-06-26 21:08:33 -0400

    created model schema for reactions inserts

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const {Schema, Types} = require('mongoose');
+const dateFormatting = require('../utils/dateFormat');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: ()=> new Types.objectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 300
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormatting(timestamp)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+module.exports= reactionSchema;
